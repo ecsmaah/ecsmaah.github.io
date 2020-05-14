dbd5918ad81a0033099cd9e0a8e30e319e493e1e
Masoud Ahmadi <ecsmaah@aim.com>
2020-05-14 14:52:17 +0200

    Add files via upload

---
+++ precache-manifest.a59beb2c592c2e68ff6c231ebc2424ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06bc98581e87702787a0eac01f5b096a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5d6a978f31eebc2275a1",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/my-app/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6a978f31eebc2275a1",
+    "url": "/my-app/static/js/main.739b439c.chunk.js"
+  },
+  {
+    "revision": "d21a59192a6355194f80",
+    "url": "/my-app/static/js/runtime-main.f5212b54.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
